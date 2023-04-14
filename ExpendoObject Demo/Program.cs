762f9a1e48d7437844624fb1cfc11685e10757c6
It Hours Solutions <It Hours Solutions@DESKTOP-LARMGGP>
2023-04-14 17:09:54 +0530

    Add project files.

---
+++ ExpendoObject Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ExpendoObject_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<String, object> dict = new Dictionary<string, object>();
+            //Dictionary<String, object> address = new Dictionary<string, object>();
+            //dict["Address"] = address;
+            //address["State"] = "WA";
+            //Console.WriteLine(((Dictionary<string, object>)dict["Address"])["State"]);
+
+            //dynamic expando = new ExpandoObject();
+            //expando.Address = new ExpandoObject();
+            //expando.Address.State = "WA";
+            //Console.WriteLine(expando.Address.State);
+
+            dynamic person = new ExpandoObject();
+            person.FirstName = "Dino";
+            person.LastName = "Esposito";
+
+            person.GetFullName = (Func<String>)(() => {
+                return String.Format("{0}, {1}",
+                  person.LastName, person.FirstName);
+            });
+
+            var name = person.GetFullName();
+            Console.WriteLine(name);
+        }
+    }
+}
